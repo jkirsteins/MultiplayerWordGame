ddbf894431f8bc03665b6884509540a09496bd3c
Jānis Kiršteins <janis@janiskirsteins.org>
2022-05-04 17:16:47 +0200

    Initial commit

---
+++ Shared/MultiplayerWordGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultiplayerWordGameApp.swift
+//  Shared
+//
+//  Created by Janis Kirsteins on 04/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct MultiplayerWordGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
