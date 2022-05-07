f16f79ae8baf1cc77ac9db42187f249e79b800a6
Janis Kirsteins <janis@janiskirsteins.org>
2022-05-07 10:50:34 +0000

    Add a modifier for flipping tiles

---
+++ Shared/Modifiers/RevealModifier.swift
@@ -0,0 +1,134 @@
+import SwiftUI
+
+struct RevealModifier<Revealed: View>: ViewModifier {
+    
+    let duration: Double
+    let callback: ()->()
+    let revealed: Revealed
+    
+    @State private var currentStep: Int = 0
+    @State private var degrees: CGFloat = 0.0
+    
+    init(
+        duration: Double, 
+        callback: @escaping ()->(), 
+        @ViewBuilder revealed: ()->Revealed) {
+            self.duration = duration 
+            self.callback = callback 
+            self.revealed = revealed()
+        }
+    
+    func degreesForStep(_ step: Int) -> CGFloat {
+        
+        max(
+            0.0, 
+            89.99 + (
+                90.0 * CGFloat(step-1)
+            )
+        )
+        
+    }
+    
+    func anim(for step: Int) -> Animation {
+        switch(step) {
+        case 1:
+            return .easeIn(duration: duration)
+        case 2:
+            return .easeOut(duration: duration)
+        default:
+            return .linear(duration: duration)
+        }
+        
+    }
+    
+    func body(content: Content) -> some View {
+        Group {
+            if currentStep == 0 || currentStep == 1 {
+                content
+            } else {
+                revealed
+                    .rotation3DEffect(
+                        .degrees(180), 
+                        axis: (1, 0, 0), 
+                        perspective: 0.0
+                    )
+            }
+        }
+        .onAnimationCompleted(for: degrees, completion: {
+            guard currentStep < 2 else {
+                callback()
+                return
+            }
+            currentStep += 1
+            withAnimation(anim(for: currentStep)) {
+                degrees = degreesForStep(currentStep)
+            }
+        })
+        .rotation3DEffect(
+            .degrees(degrees), 
+            axis: (1, 0, 0), 
+            perspective: 0.0
+        )
+        .onAppear {
+            currentStep = 1
+            withAnimation(anim(for: currentStep)) 
+            {
+                degrees = degreesForStep(currentStep)
+            }
+        }
+    }
+}
+
+extension View {
+    //    func blinking(duration: Double = 0.75) -> some View {
+    //        modifier(BlinkViewModifier(duration: duration))
+    //    }
+}
+
+struct RevealModifierTestView: View {
+    let from = Letter("A", points: 5)
+    let to = Letter("B", points: 2)
+    
+    @State var flip = false
+    @State var uuid = UUID()
+    
+    var body: some View {
+        VStack(spacing: 24) {
+            if !flip {
+                TileInHand(
+                    letter: from, 
+                    highlight: true)
+            } 
+            
+            if flip {
+                TileInHand(
+                    letter: from, 
+                    highlight: true)
+                    .modifier(
+                        RevealModifier(
+                            duration: 0.5, 
+                            callback: {
+                                flip = false
+                                uuid = UUID()
+                            },
+                            revealed: {
+                                TileInHand(
+                                    letter: to, 
+                                    highlight: false)
+                            }
+                        )
+                    )
+            }
+            
+            Button("Flip") {
+                flip = true
+            }
+        }.id(uuid)
+    }
+}
+
+struct RevealModifier_Previews: PreviewProvider {
+    static var previews: some View {
+        RevealModifierTestView()
+    }
+}
